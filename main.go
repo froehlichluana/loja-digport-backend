c531d0fb285e436f1817abc7c28233ca9f922fcf
froehlichluana <froehlichluana@gmail.com>
2024-04-25 16:09:00 -0300

    create get and post

--- main.go
+++ main.go
@@ -1,14 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
-
 func main() {
 	StartServer()
-
-	fmt.Println("Hello, world!")
-	
-	fmt.Printf("Esse é o catálogo da loja Neon Bastet: +%v", createCatalog())
 }
--- model/produto.go
+++ model/produto.go
@@ -1,11 +1,11 @@
 package model
 
 type Product struct{
-	Name string
-	Description string
-	Category string
-	Id string
-	Price float64
-	Quantity int
-	Image string
+	Name string `json:"name"`
+	Description string `json:"description"`
+	Category string `json:"category"`
+	Id string `json:"id"`
+	Price float64 `json:"price"`
+	Quantity int `json:"quantity"`
+	Image string `json:"image"`
 }
